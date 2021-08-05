ee5c0df40e5606664d8629d2815897e695090ba1
LaimyDev <74705080+LaimyDev@users.noreply.github.com>
2021-08-05 21:46:59 +0200

    Add files via upload

---
+++ foodshop/backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Chicken Akhini',
+    image: '/images/chicken.jpg',
+    description:
+      'A South African-Indian authentic , fragrant and highly flavoursome chicken and rice dish filled with soft potatoes. R70 per Kg',
+    brand: 'Chicken',
+    category: 'Food',
+    price: 70.00,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Lamb Akhini',
+    image: '/images/lamb.png',
+    description:
+      'A South African-Indian authentic , fragrant and highly flavoursome lamb and rice dish filled with soft potatoes. R85 per Kg',
+    brand: 'Meat',
+    category: 'Food',
+    price: 85.00,
+    countInStock: 5,
+    rating: 5,
+    numReviews: 8,
+  },
+  {
+    name: 'Onion Salad',
+    image: '/images/dayandsly.jpg',
+    description:
+      'All our meals are best served with a side of day and sly. A cold and refreshing yoghurt based sauce and a sweet and sour onion salad. R10 for a 50mL container',
+    brand: 'Addon',
+    category: 'Food',
+    price: 10.00,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'Date and Walnut loaf',
+    image: '/images/dateloaf.jpg',
+    description:
+      'Perfect for tea-time on a Sunday afternoon. A light and fluffy loaf filled with pieces of Cadbury chocolate and crunchy walnuts. R95 per loaf',
+    brand: 'Dessert',
+    category: 'Food',
+    price: 80.00,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Sorgi',
+    image: '/images/sorgi.jpg',
+    description:
+      'An Indian sweet treat served before meals. Made with semolina, butter, milk and cardamom. It is simple yet decadent and a dessert. R25 for 250g',
+    brand: 'Addon',
+    category: 'Food',
+    price: 20.00,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 10,
+  },
+  {
+    name: 'Creamy Yoghurt',
+    image: '/images/dayandsly.jpg',
+    description:
+      'All our meals are best served with a side of day and sly. A cold and refreshing yoghurt based sauce and a sweet and sour onion salad. R10 for a 50mL container',
+    brand: 'Addon',
+    category: 'Food',
+    price: 20.00,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
